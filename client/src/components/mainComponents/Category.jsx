271c68bc9e20b2fe329c7dab75ed4910ef6115aa
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-06-18 15:56:28 -0700

    hover effects for categories

--- client/src/components/mainComponents/Category.jsx
+++ client/src/components/mainComponents/Category.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Title = styled.div`
+  visibility: hidden;
+  color: black;
+  font-size: 30px;
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const ImgContainer = styled.div`
+`
 
 const CategoryStyle = styled.div`
   max-width: 375px;
   margin: 2.5px;
+  position: relative;
+  z-index: 0;
+
+  :hover ${Title} {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  :hover ${ImgContainer} {
+    opacity: .33;
+  }
   
   :hover {
-		opacity: .33;
-		cursor: pointer;
-	}
+    cursor: pointer;
+  }
 `
+
+
 
 const Category = (props) => {
 
   return (
     <CategoryStyle>
-      <img src={props.image} alt="" width="370"/>
+      <ImgContainer>
+        <img src={props.image} alt="" width="370"/>
+      </ImgContainer>
+      <Title>{props.name}</Title>
     </CategoryStyle>
   )
 }
